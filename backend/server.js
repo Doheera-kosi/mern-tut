6c15449ae311cf88263bf2ce9b78e77c6f27de1b
Doheera-kosi <kupourevans99@gmail.com>
2024-02-15 08:39:17 +0000

    Add express server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const PORT = 5000
+const port = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+app.listen(port, () => console.log(`Server started on port: ${port}`));
